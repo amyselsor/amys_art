a8ed3d6852af67c5c621e79f165c8d04617976b9
Amy Selsor <a_sels@me.com>
2012-07-11 22:30:04 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmysArt::Application.config.secret_token = '7c6c9026fa03a6bf7e0dd7a7d07777473ffa65d8c5b7ee9f0e913b20f73bf7c8747cd0f4472a6945a530aca2ce34ca95dd5937727056993517b5146597c09835'
